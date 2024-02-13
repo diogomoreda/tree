6aac2bbae78ab9016ffb18a5b56ea3d0cd648e3c
mindthefrequency <diogomoreda@gmail.com>
2024-02-13 03:42:21 +0000

    cleanup

--- src/classes/tree-tree-ui-actions.js
+++ src/classes/tree-tree-ui-actions.js
@@ -1,6 +1,6 @@
 Tree.Classes.TreeUI.prototype.actions = function() {}
 
-
+// private
 Tree.Classes.TreeUI.prototype.addItem = function(parentNodeUI, type) {
     var parentId = !parentNodeUI ? null : parentNodeUI.model.id;
     
@@ -24,21 +24,21 @@ Tree.Classes.TreeUI.prototype.addItem = function(parentNodeUI, type) {
     this.buildModelUI();
 }
 
-
+// public
 Tree.Classes.TreeUI.prototype.addFolder = function(parentNodeUI) {
     if (parentNodeUI) parentNodeUI = this.getNodeFolder(parentNodeUI);
     if (parentNodeUI) parentNodeUI.model.open = true;
     this.addItem(parentNodeUI, 0);
 }
 
-
+// public
 Tree.Classes.TreeUI.prototype.addFile = function(parentNodeUI) {
     if (parentNodeUI) parentNodeUI = this.getNodeFolder(parentNodeUI);
     if (parentNodeUI) parentNodeUI.model.open = true;
     this.addItem(parentNodeUI, 1);
 }
 
-
+// public
 Tree.Classes.TreeUI.prototype.renameItem = function(nodeUI) {
     this.edit = true;
     this.actionFlags.editNodeId = nodeUI.model.id;
@@ -46,7 +46,7 @@ Tree.Classes.TreeUI.prototype.renameItem = function(nodeUI) {
     this.buildModelUI();
 }
 
-
+// public
 Tree.Classes.TreeUI.prototype.finishEdit = function() {
     this.clearModelSelectedNodes();
     var nodeUI = this.getNodeUI(this.actionFlags.editNodeId);
@@ -57,7 +57,7 @@ Tree.Classes.TreeUI.prototype.finishEdit = function() {
     this.buildModelUI();
 }
 
-
+// private
 Tree.Classes.TreeUI.prototype.fillClipboard = function() {
     // clear the clipboard
     this.clipboard = [];
@@ -88,17 +88,17 @@ Tree.Classes.TreeUI.prototype.fillClipboard = function() {
         // restore the original value of the selected node UI parent node ID
         selectedNodeUI.model.parent = selectedNodeParentId;
     }    
-    console.log('clipboard filled:', this.clipboard);
+    //console.log('clipboard filled:', this.clipboard);
 };
 
-
+// public
 Tree.Classes.TreeUI.prototype.cutItems = function() {
     this.actionFlags.isCutOperation = true;
     this.fillClipboard();
     this.buildModelUI();
 }
 
-
+// public
 Tree.Classes.TreeUI.prototype.copyItems = function() {
     this.clearModelCutState();
     this.actionFlags.isCutOperation = false;
@@ -106,7 +106,7 @@ Tree.Classes.TreeUI.prototype.copyItems = function() {
     this.buildModelUI();
 }
 
-
+// public
 Tree.Classes.TreeUI.prototype.pasteItems = function(parentNodeUI) {
     if (parentNodeUI) parentNodeUI = this.getNodeFolder(parentNodeUI);
     if (parentNodeUI) parentNodeUI.model.open = true;
@@ -118,7 +118,7 @@ Tree.Classes.TreeUI.prototype.pasteItems = function(parentNodeUI) {
     for (var i=0; i<this.clipboard.length; i++) partialModel.push(JSON.parse(this.clipboard[i]));
     // filter the partial model through a cut or copy operation
     partialModel = this.actionFlags.isCutOperation ? this.pasteFromCut(partialModel) : this.pasteFromCopy(partialModel);
-    console.log(partialModel);
+    //console.log(partialModel);
     // loop trhough the partial model
     for (var i=0; i<partialModel.length; i++) {
         if (!partialModel[i].type) partialModel[i].open = true;
@@ -146,7 +146,7 @@ Tree.Classes.TreeUI.prototype.pasteItems = function(parentNodeUI) {
     this.buildModelUI();
 }
 
-
+// private
 Tree.Classes.TreeUI.prototype.pasteFromCut = function(partialModel) {
     // clear the clipboard to disable successive paste operation from one cut
     this.clipboard = null;
@@ -164,7 +164,7 @@ Tree.Classes.TreeUI.prototype.pasteFromCut = function(partialModel) {
     return partialModel;
 }
 
-
+// private
 Tree.Classes.TreeUI.prototype.pasteFromCopy = function(partialModel) {
     // create a local idCtr to aid in calculating the next available nodeId, for new nodes that are copied from existing ones.
     var idCtr = 1;
@@ -226,7 +226,7 @@ Tree.Classes.TreeUI.prototype.pasteFromCopy = function(partialModel) {
     return output;
 }
 
-
+// public
 Tree.Classes.TreeUI.prototype.deleteItems = function() {
     var nodeRemoveList = [];
     for (var i=0; i<this.nodeUIs.length; i++) {
@@ -255,175 +255,3 @@ Tree.Classes.TreeUI.prototype.deleteItems = function() {
     this.model = newModel;
     this.buildModelUI();
 }
-
-
-
-
-
-/** 
- *  {
- *    id: '0000-0001',           1. gets copied and becomes '0000-0007'
- *    parent: null,              2. the node copy parent is set to null 
- *    children: [                3. children are kept the same for now
- *      '0000-0002',
- *      '0000-0006'
- *    ]
- *  },
- *  {
- *    id: '0000-0002',
- *    parent: '0000-0001',       2. parent is replaced
- *    children: [
- *      '0000-0003'
- *    ]
- *  },
- *  {
- *    id: '0000-0003',
- *    parent: '0000-0002',
- *    children: [
- *      '0000-0004',
- *      '0000-0005',
- *    ]
- *  },
- *  {
- *    id: '0000-0004',
- *    parent: '0000-0003',
- *    children: null
- *  },
- *  {
- *    id: '0000-0005',
- *    parent: '0000-0003',
- *    children: null
- *  },
- *  {
- *    id: '0000-0006',
- *    parent: '0000-0001',     2. parent is replaced
- *    children: null
- *  },
- * **/
-//if (rootNodeCopy.children)
-//this.copyNodeChildren()
-/*
-Tree.Classes.TreeUI.prototype.copyNodeChildren = function(nodeModel, usedNodeIds, idCtr=1) {
-    var nodeChildren = [];
-    for (var i=0; i<nodeModel.children.length; i++) {
-        // get the next available node Id
-        while (usedNodeIds.includes(idCtr)) idCtr++;
-        // add it to the list of used Ids
-        usedNodeIds.push(idCtr);
-        // convert the id value to a string format
-        var newId = Tree.intToId(idCtr);
-        // get the nodeUI from the id in nodeModel.children
-        var childNodeUI = this.getNodeUI(nodeModel.children[i]);
-        // generate a copy of the child Node Model: assign a new id, and assign the nodeModel as the 
-        var nodeCopy = Tree.copyNode(newId, childNodeUI.model, nodeModel.id);
-        // update the nodeModel.children item at this index
-        nodeModel.children[i] = newId;  
-        // add the copied node to the output array
-        nodeChildren.push(nodeCopy);
-        // if the copied node also contains children, then repeat the process
-        if (nodeCopy.children) {
-            nodeChildren = nodeChildren.concat(this.copyNodeChildren(nodeCopy, usedNodeIds, idCtr));
-        }
-    }
-    return nodeChildren;
-}
-*/
-/*
-    // disable the 'highlihted' and 'disabled' states for all nodes
-    this.cutItemsCancel();
-    // create a local idCtr to aid in calculating the next available nodeId, for new nodes that are copied from existing ones.
-    var idCtr = 1;
-    // get a local copy of all ID INT values used by the existing nodes, to aid in calculating the next available nodeId
-    var usedNodeIds = this.getUsedNodeIds();
-    // clear the clipboard
-    this.clipboard = [];
-    // create an array to keep track of processed node IDs
-    var clipboardNodeIds = [];
-    // create a reference to the local treeUI scope 
-    var _self = this;
-    // method to recursively copy node children
-    function copyNodeChildren(nodeModel) {
-        var nodeChildren = [];
-        for (var i=0; i<nodeModel.children.length; i++) {
-            // get the next available node Id
-            while (usedNodeIds.includes(idCtr)) idCtr++;
-            // add it to the list of used Ids
-            usedNodeIds.push(idCtr);
-            // convert the id value to a string format
-            var newId = Tree.intToId(idCtr);
-            // get the nodeUI from the id in nodeModel.children
-            var childNodeUI = _self.getNodeUI(nodeModel.children[i]);
-            // generate a copy of the child Node Model: assign a new id, and assign the nodeModel as the 
-            var nodeCopy = Tree.copyNode(newId, childNodeUI.model, nodeModel.id);
-            // update the nodeModel.children item at this index
-            nodeModel.children[i] = newId;  
-            // add the copied node to the output array
-            nodeChildren.push(nodeCopy);
-            // if the copied node also contains children, then repeat the process
-            if (nodeCopy.children) {
-                nodeChildren = nodeChildren.concat(copyNodeChildren(nodeCopy));
-            }
-        }
-        return nodeChildren;
-    }
-    // loop through every node in the tree
-    for (var i=0; i<this.nodeUIs.length; i++) {
-        // only process nodes that are selected
-        if (!this.nodeUIs[i].model.selected) continue;
-        // ensure that the selected node is in the lowest level of the selection hierarchy (the selected node, closest to the tree root node) 
-        var selectedNodeUI = this.getNodeUISelectionRoot(this.nodeUIs[i]);
-        // add the node id to the list of processed nodes
-        if (clipboardNodeIds.includes(selectedNodeUI.model.id)) continue;
-        clipboardNodeIds.push(selectedNodeUI.model.id);
-        // get the next available node Id
-        while (usedNodeIds.includes(idCtr)) idCtr++;
-        // add it to the local list of used Ids
-        usedNodeIds.push(idCtr);
-        // convert the id value to an id string format 
-        var newId = Tree.intToId(idCtr);
-        // copy the selected node into the nodesCopy array, the node copy has its parent set to null
-        var nodesCopy = [Tree.copyNode(newId, this.nodeUIs[i].model, null)]
-        // if the selected node contains children, get the children nodeUIs recursively, and create copies to add to the array
-        if (nodesCopy[0].children) nodesCopy = nodesCopy.concat(copyNodeChildren(nodesCopy[0]));
-        // copy copied nodes to the clipboard, as JSON strings
-        for (var e=0; e<nodesCopy.length; e++) this.clipboard.push(JSON.stringify(nodesCopy[e]));
-        
-    }
-    console.log(clipboardNodeIds);
-    console.log(this.clipboard);
-    this.actionFlags.isCutOperation = false;
-    this.buildModelUI();
-    */
-   /*
-    // clear the clipboard
-    this.clipboard = [];
-    // create an array to keep track of processed node IDs
-    var clipboardNodeIds = [];
-    // loop through every node in the tree
-    for (var i=0; i<this.nodeUIs.length; i++) {
-        // only process nodes that are selected
-        if (!this.nodeUIs[i].model.selected) continue;
-        // ensure that the selected node is in the lowest level of the selection hierarchy (the selected node, closest to the tree root node) 
-        var selectedNodeUI = this.getNodeUISelectionRoot(this.nodeUIs[i]);
-        // add the node id to the list of processed nodes
-        if (clipboardNodeIds.includes(selectedNodeUI.model.id)) continue;
-        clipboardNodeIds.push(selectedNodeUI.model.id);
-        // copy the selected node into the nodesCopy array, the node copy has its parent set to null
-        var nodesCopy = [{model: Tree.copyNode(selectedNodeUI.model.id, selectedNodeUI.model, null)}];
-        // if the selected node contains children, get the children nodeUIs recursively, and add their references to the array
-        if (nodesCopy[0].model.children) nodesCopy = nodesCopy.concat(this.getNodeUIChildren(nodesCopy[0]));
-        // copy data to clipboard and update nodeUI visual state
-        for (var e=0; e<nodesCopy.length; e++) {
-            // copy nodeUI references to the clipboard, as JSON strings
-            this.clipboard.push(JSON.stringify(nodesCopy[e].model));
-            // update the selected nodeUI and its children nodeUIs visual state
-            nodesCopy[e].model.selected = false;
-            nodesCopy[e].model.highlighted = true;
-            nodesCopy[e].model.disabled = true;    
-        }
-    }
-    console.log(clipboardNodeIds);
-    console.log(this.clipboard);
-    this.actionFlags.isCutOperation = true;
-    this.buildModelUI();
-    */
